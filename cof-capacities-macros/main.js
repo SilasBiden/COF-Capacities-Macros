50aec9088e8c5588c1bb2a9ed5d62b264f242e5c
SilasBiden <104033283+SilasBiden@users.noreply.github.com>
2022-04-27 14:36:29 +0200

    Correct COF path

--- cof-capacities-macros/main.js
+++ cof-capacities-macros/main.js
@@ -1,21 +1,21 @@
 // Import Modules
-import { CofActor } from "./actors/actor.js";
-import { CofItem } from "./items/item.js";
+import { CofActor } from "../../systems/cof/module/actors/actor.js";
+import { CofItem } from "../../systems/cof/module/items/item.js";
 
-import { CofItemSheet } from "./items/item-sheet.js";
-import { CofActorSheet } from "./actors/actor-sheet.js";
+import { CofItemSheet } from "../../systems/cof/module/items/item-sheet.js";
+import { CofActorSheet } from "../../systems/cof/module/actors/actor-sheet.js";
 
-import { preloadHandlebarsTemplates } from "./system/templates.js";
-import { registerHandlebarsHelpers } from "./system/helpers.js";
-import { registerSystemSettings } from "./system/settings.js";
+import { preloadHandlebarsTemplates } from "../../systems/cof/module/system/templates.js";
+import { registerHandlebarsHelpers } from "../../systems/cof/module/system/helpers.js";
+import { registerSystemSettings } from "../../systems/cof/module/system/settings.js";
 
-import { System, COF } from "./system/config.js";
-import { Macros } from "./system/macros.js";
-import registerHooks from "./system/hooks.js";
-import { CofLootSheet } from "./actors/loot-sheet.js";
-import { COFActiveEffectConfig } from "./system/active-effect-config.js";
-import { EffectsModifications, customizeStatusEffects } from "./effects/effects.js";
-import { CapaMacros } = "../../systems/cof/module/system/macros".js
+import { System, COF } from "../../systems/cof/module/system/config.js";
+import { Macros } from "../../systems/cof/module/system/macros.js";
+import registerHooks from "../../systems/cof/module/system/hooks.js";
+import { CofLootSheet } from "../../systems/cof/module/actors/loot-sheet.js";
+import { COFActiveEffectConfig } from "../../systems/cof/module/system/active-effect-config.js";
+import { EffectsModifications, customizeStatusEffects } from "../../systems/cof/module/effects/effects.js";
+import { CapaMacros } = "./macros/macro".js
 
 
 Hooks.once("init", async function () {
